f18e9a4b1ef388a01b37ca583bd0ce3ebd945f8d
Egil <egil.moller@freecode.no>
2022-10-12 23:27:12 +0200

    Added example data

---
+++ data.example/story.js
@@ -0,0 +1,15 @@
+define([], function () {
+    return [
+      {"url": "data/images/2009-04-24 37600751_2179788432266157_7647236476513550336_o.jpg", "date": "2009-04-24", "lat": 59.917537, "lon": 10.770006, "type": "home"},
+      {"url": "data/images/2014-12 39946889_662052144174406_4350902257118609408_n.jpg", "date": "2014-12", "lat": 59.307049, "lon": 18.117053},
+      {"url": "data/images/2014-12 40572921_889802334547739_5220050384805953536_n.jpg", "date": "2014-12", "lat": 59.307049, "lon": 18.117053},
+      {"url": "data/images/2017-09-22 IMG_20170922_221655.jpg", "date": "2017-09-22", "lat": 69.662063, "lon": 18.964962, "type": "home"},        
+      {"html": "\
+    <div class=\"intro centered\">\
+      <div><img src=\"img/logo.red.svg\" style=\"width: 40%; margin-top: -40pt; margin-bottom: -60pt;\"></div>\
+      <h1 style=\"color: #990000;\">StoryMode</h1>\
+      <div style=\"font-size: 32pt;\">Timeline and map viewer</div>\
+    </div>\
+", "date": "2018-01-01", "lat": 69.652089, "lon": 18.974519}
+    ];
+});
